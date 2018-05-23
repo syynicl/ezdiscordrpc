2cbb32c90db09b6a9961206b81a0d66127710a4c
syynicl <syynicl@gmail.com>
2018-05-23 18:41:45 +0100

    Added custom KeyboardInterrupt message.

--- ezdiscordrpc.py
+++ ezdiscordrpc.py
@@ -1,16 +1,20 @@
-# Import modules
-from pypresence import Presence
-import time
+try:
+    # Import modules
+    from pypresence import Presence
+    import time
 
-# Connect to Discord
-cid = int(input('RPC Client ID: '))
-rpc = Presence(cid)
-rpc.connect()
+    # Connect to Discord
+    cid = int(input('RPC Client ID: '))
+    rpc = Presence(cid)
+    rpc.connect()
 
-# Set RPC
-rpc.update(small_image = 'small', large_image = 'large')
-print('RPC set successfully.')
+    # Set RPC
+    rpc.update(small_image = 'small', large_image = 'large')
+    print('RPC set successfully.')
 
-# Update RPC every 15 seconds
-while True:
-    time.sleep(15)
+    # Update RPC every 15 seconds
+    while True:
+        time.sleep(15)
+
+except KeyboardInterrupt:
+    print('\nProgram stopped using Ctrl+C.')
